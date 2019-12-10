161ae8d2d73a9a97b32bfe5a3cf5093ffeb41077
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2019-12-10 12:51:54 -0600

    Add files via upload

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    render() {
+      return (
+        <form className="Search">
+          <button className="addbutton">Add item to Inventory</button>
+
+          <input type="text" placeholder="Search by -->" />
+          <select id = "myList">
+               <option value = "1">Brand</option>
+               <option value = "2">Flavor</option>
+               <option value = "3">Size</option>
+             </select>
+          <button>Search</button>
+        </form>
+      );
+    }
+  }
+
+export default SearchBar;
